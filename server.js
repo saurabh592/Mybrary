6294a792d729ba1541d99c35a46f8227bc65da52
saurabh592 <shubham.pathak9323@gmail.com>
2022-03-27 13:45:43 +0530

    make it better

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ app.set('views',__dirname+'/views');
 app.set('layout','layouts/layout');
 app.use(expressLayouts)
 app.use(express.static('public'));
-
-mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser:true});
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true})
+//mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser:true});
 const db =mongoose.connection;
 db.on('error',error =>console.error(error));
 db.once('open',() => console.log("coonected to database"));
