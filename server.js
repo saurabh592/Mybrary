a447a21f013dd6eeb4e124a8241abe440599cfbd
saurabh592 <shubham.pathak9323@gmail.com>
2022-03-27 14:39:11 +0530

    trying 4th time

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.set('layout','layouts/layout');
 app.use(expressLayouts)
 app.use(express.static('public'));
 
-mongoose.connect( process.env.DATABASE_URL,{ useNewUrlParser: true})
+mongoose.connect(`${ process.env.DATABASE_URL} `,{ useNewUrlParser: true})
 //mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser:true});
 const db =mongoose.connection;
 db.on('error',error =>console.error(error));
