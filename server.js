71be053e656ed688650e8a62bd56f45adfed1451
saurabh592 <shubham.pathak9323@gmail.com>
2022-03-27 14:56:48 +0530

    6th time

--- server.js
+++ server.js
@@ -6,7 +6,10 @@ const express = require("express");
 const app = express();
 const mongoose = require('mongoose');
 const expressLayouts = require("express-ejs-layouts");
+const dotenv = require('dotenv');
+dotenv.config(options{path:'./.env'})
 const indexRouter = require('./routes/index');
+const { options } = require('./routes/index');
 
 
 app.set('view engine','ejs');
