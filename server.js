b3ca9e5cc334adc16ec49ef62ffbfe10b3ee5790
saurabh592 <shubham.pathak9323@gmail.com>
2022-03-27 14:10:01 +0530

    make it better

--- server.js
+++ server.js
@@ -4,15 +4,17 @@ if(process.env.NODE_ENV !== 'production'){
 
 const express = require("express");
 const app = express();
+const mongoose = require('mongoose');
 const expressLayouts = require("express-ejs-layouts");
 const indexRouter = require('./routes/index');
-const mongoose = require('mongoose');
+
 
 app.set('view engine','ejs');
 app.set('views',__dirname+'/views');
 app.set('layout','layouts/layout');
 app.use(expressLayouts)
 app.use(express.static('public'));
+
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true})
 //mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser:true});
 const db =mongoose.connection;
