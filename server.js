664047da90fa9180fdb7adac0fbdfbc0ea9af8ef
saurabh592 <shubham.pathak9323@gmail.com>
2022-03-27 14:26:59 +0530

    hello

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-// if(process.env.NODE_ENV !== 'production'){
-//     require('dotenv').config({ path: '.env' })
-// }
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config()
+}
 
 const express = require("express");
 const app = express();
@@ -15,7 +15,7 @@ app.set('layout','layouts/layout');
 app.use(expressLayouts)
 app.use(express.static('public'));
 
-mongoose.connect("mongodb+srv://localhost/mybrary", { useNewUrlParser: true})
+mongoose.connect( process.env.DATABASE_URL,{ useNewUrlParser: true})
 //mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser:true});
 const db =mongoose.connection;
 db.on('error',error =>console.error(error));
